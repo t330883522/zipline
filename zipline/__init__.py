212dfaf95f1b4835199ea8ffd8f01e8e331716f6
n1 <liu.dengfeng@hotmail.com>
2017-12-27 21:21:06 +0800

    1.2.0

--- zipline/__init__.py
+++ zipline/__init__.py
@@ -43,7 +43,7 @@ if global_calendar_dispatcher._calendars:
 del global_calendar_dispatcher
 
 
-__version__ = get_versions()['version']
+__version__ = '1.2.0' #get_versions()['version']
 del get_versions
 
 
